a66b0f557d0ae0bacb2af6034e5c8b4d3c22f2b8
drumber-1 <drumber01@gmail.com>
2016-03-13 23:29:06 +0000

    Added many test textures

--- src/test_game2/TestGame2.cpp
+++ src/test_game2/TestGame2.cpp
@@ -26,13 +26,65 @@ std::vector<std::string> loadModels() {
 
 std::vector<Material> loadMaterials() {
 	Texture::textureManager.emplace("test.png");
-	Texture::textureManager.emplace("bricks2.jpg");
-	Texture::textureManager.emplace("bricks2_normal.jpg");
+	Texture::textureManager.emplace("bricks.jpg");
+	Texture::textureManager.emplace("bricks_normal.jpg");
+	Texture::textureManager.emplace("adesert_mntn4_d.jpg");
+	Texture::textureManager.emplace("adesert_mntn4_n.jpg");
+	Texture::textureManager.emplace("adesert_mntn4_s.jpg");
+	Texture::textureManager.emplace("desert_cracks_d.jpg");
+	Texture::textureManager.emplace("desert_cracks_n.jpg");
+	Texture::textureManager.emplace("desert_cracks_s.jpg");
+	Texture::textureManager.emplace("desert_sand_big_d.jpg");
+	Texture::textureManager.emplace("desert_sand_big_n.jpg");
+	Texture::textureManager.emplace("desert_sand_big_s.jpg");
+	Texture::textureManager.emplace("desert_sand_d.jpg");
+	Texture::textureManager.emplace("desert_sand_n.jpg");
+	Texture::textureManager.emplace("desert_sand_s.jpg");
+	Texture::textureManager.emplace("grass_autumn_d.jpg");
+	Texture::textureManager.emplace("grass_autumn_n.jpg");
+	Texture::textureManager.emplace("grass_autumn_s.jpg");
+	Texture::textureManager.emplace("grass_rocky_d.jpg");
+	Texture::textureManager.emplace("grass_rocky_n.jpg");
+	Texture::textureManager.emplace("grass_rocky_s.jpg");
+	Texture::textureManager.emplace("ground_cracked_d.jpg");
+	Texture::textureManager.emplace("ground_cracked_n.jpg");
+	Texture::textureManager.emplace("ground_cracked_s.jpg");
+	Texture::textureManager.emplace("jungle_stone_d.jpg");
+	Texture::textureManager.emplace("jungle_stone_n.jpg");
+	Texture::textureManager.emplace("jungle_stone_s.jpg");
+	Texture::textureManager.emplace("lava_d.jpg");
+	Texture::textureManager.emplace("lava_n.jpg");
+	Texture::textureManager.emplace("lava_s.jpg");
+	Texture::textureManager.emplace("moss_plants_d.jpg");
+	Texture::textureManager.emplace("moss_plants_n.jpg");
+	Texture::textureManager.emplace("moss_plants_s.jpg");
+	Texture::textureManager.emplace("snow_bumpy_d.jpg");
+	Texture::textureManager.emplace("snow_bumpy_n.jpg");
+	Texture::textureManager.emplace("snow_bumpy_s.jpg");
+	Texture::textureManager.emplace("snow_d.jpg");
+	Texture::textureManager.emplace("snow_n.jpg");
+	Texture::textureManager.emplace("snow_s.jpg");
+	Texture::textureManager.emplace("snow_grass_d.jpg");
+	Texture::textureManager.emplace("snow_grass_n.jpg");
+	Texture::textureManager.emplace("snow_grass_s.jpg");
+
 
 	std::vector<Material> materials;
 	materials.emplace_back(Texture::textureManager.getPointer("test.png"), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
-	materials.emplace_back(Texture::textureManager.getPointer("bricks2.jpg"), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 0, 0, Texture::textureManager.getPointer("bricks2_normal.jpg"));
-	materials.emplace_back(Texture::textureManager.getPointer("bricks2.jpg"), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 0, 0);
+	materials.emplace_back(Texture::textureManager.getPointer("bricks.jpg"), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 0, 0, Texture::textureManager.getPointer("bricks_normal.jpg"));
+	materials.emplace_back(Texture::textureManager.getPointer("adesert_mntn4_d.jpg"), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 1, 32, Texture::textureManager.getPointer("adesert_mntn4_n.jpg"), Texture::textureManager.getPointer("adesert_mntn4_s.jpg"));
+	materials.emplace_back(Texture::textureManager.getPointer("desert_cracks_d.jpg"), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 1, 32, Texture::textureManager.getPointer("desert_cracks_n.jpg"), Texture::textureManager.getPointer("desert_cracks_s.jpg"));
+	materials.emplace_back(Texture::textureManager.getPointer("desert_sand_big_d.jpg"), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 1, 32, Texture::textureManager.getPointer("desert_sand_big_n.jpg"), Texture::textureManager.getPointer("desert_sand_big_s.jpg"));
+	materials.emplace_back(Texture::textureManager.getPointer("desert_sand_d.jpg"), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 1, 32, Texture::textureManager.getPointer("desert_sand_n.jpg"), Texture::textureManager.getPointer("desert_sand_s.jpg"));
+	materials.emplace_back(Texture::textureManager.getPointer("grass_autumn_d.jpg"), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 1, 32, Texture::textureManager.getPointer("grass_autumn_n.jpg"), Texture::textureManager.getPointer("grass_autumn_s.jpg"));
+	materials.emplace_back(Texture::textureManager.getPointer("grass_rocky_d.jpg"), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 1, 32, Texture::textureManager.getPointer("grass_rocky_n.jpg"), Texture::textureManager.getPointer("grass_rocky_s.jpg"));
+	materials.emplace_back(Texture::textureManager.getPointer("ground_cracked_d.jpg"), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 1, 32, Texture::textureManager.getPointer("ground_cracked_n.jpg"), Texture::textureManager.getPointer("ground_cracked_s.jpg"));
+	materials.emplace_back(Texture::textureManager.getPointer("jungle_stone_d.jpg"), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 1, 32, Texture::textureManager.getPointer("jungle_stone_n.jpg"), Texture::textureManager.getPointer("jungle_stone_s.jpg"));
+	materials.emplace_back(Texture::textureManager.getPointer("lava_d.jpg"), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 1, 32, Texture::textureManager.getPointer("lava_n.jpg"), Texture::textureManager.getPointer("lava_s.jpg"));
+	materials.emplace_back(Texture::textureManager.getPointer("moss_plants_d.jpg"), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 1, 32, Texture::textureManager.getPointer("moss_plants_n.jpg"), Texture::textureManager.getPointer("moss_plants_s.jpg"));
+	materials.emplace_back(Texture::textureManager.getPointer("snow_bumpy_d.jpg"), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 1, 32, Texture::textureManager.getPointer("snow_bumpy_n.jpg"), Texture::textureManager.getPointer("snow_bumpy_s.jpg"));
+	materials.emplace_back(Texture::textureManager.getPointer("snow_d.jpg"), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 1, 32, Texture::textureManager.getPointer("snow_n.jpg"), Texture::textureManager.getPointer("snow_s.jpg"));
+	materials.emplace_back(Texture::textureManager.getPointer("snow_grass_d.jpg"), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 1, 32, Texture::textureManager.getPointer("snow_grass_n.jpg"), Texture::textureManager.getPointer("snow_grass_s.jpg"));
 	return materials;
 }
 
@@ -48,9 +100,9 @@ TestGame2::TestGame2(Engine* engine) : Game(engine) {
 	m_rootEntity.addChildEntity(camera);
 	m_currentCamera = cameraComponent;
 
-	/*Entity* cameraLight = new Entity();
+	Entity* cameraLight = new Entity();
 	cameraLight->addComponent(new PointLightComponent(glm::vec3(1.0, 1.0, 1.0), 0.8f, 100));
-	camera->addChildEntity(cameraLight);*/
+	camera->addChildEntity(cameraLight);
 
 	Entity* plane = new Entity();
 	plane->addComponent(new RenderComponent(Mesh::meshManager.getPointer("plane"), materials[0]));
@@ -70,7 +122,7 @@ TestGame2::TestGame2(Engine* engine) : Game(engine) {
 	probe->addComponent(new MovableComponent());
 	m_rootEntity.addChildEntity(probe);
 
-	/*std::srand((unsigned int)std::time(0));
+	std::srand((unsigned int)std::time(0));
 	for (int i = 0; i <= 10; i++) {
 		for (int j = 0; j <= 10; j++) {
 			float x = (i - 5) * 2.0f;
@@ -79,33 +131,11 @@ TestGame2::TestGame2(Engine* engine) : Game(engine) {
 
 			Entity* object = new Entity();
 			object->getLocalTransform().translate(glm::vec3(x, y, z));
-			std::string model = models[std::rand() % (models.size() - 1)];
-			Material material = materials[std::rand() % materials.size()];
+			std::string model = models[(i + 11*j) % (models.size() - 1)];
+			Material material = materials[(i + 11*j) % materials.size()];
 			object->addComponent(new RenderComponent(Mesh::meshManager.getPointer(model), material));
 			m_rootEntity.addChildEntity(object);
 
 		}
-	}*/
-
-	Entity* cube1 = new Entity();
-	cube1->getLocalTransform().translate(glm::vec3(-1.5, 1.0, -2.0));
-	cube1->addComponent(new RenderComponent(Mesh::meshManager.getPointer(models[0]), materials[1]));
-	m_rootEntity.addChildEntity(cube1);
-
-	Entity* cube2 = new Entity();
-	cube2->getLocalTransform().translate(glm::vec3(1.5, 1.0, -2.0));
-	cube2->addComponent(new RenderComponent(Mesh::meshManager.getPointer(models[0]), materials[2]));
-	m_rootEntity.addChildEntity(cube2);
-
-	Entity* cube3 = new Entity();
-	cube3->getLocalTransform().translate(glm::vec3(-1.5, 1.0, -5.0));
-	cube3->getLocalTransform().rotate(glm::radians(45.0f), glm::vec3(0, 1, 0));
-	cube3->addComponent(new RenderComponent(Mesh::meshManager.getPointer(models[0]), materials[1]));
-	m_rootEntity.addChildEntity(cube3);
-
-	Entity* cube4 = new Entity();
-	cube4->getLocalTransform().translate(glm::vec3(1.5, 1.0, -5.0));
-	cube4->getLocalTransform().rotate(glm::radians(45.0f), glm::vec3(0, 1, 0));
-	cube4->addComponent(new RenderComponent(Mesh::meshManager.getPointer(models[0]), materials[2]));
-	m_rootEntity.addChildEntity(cube4);
+	}
 }
